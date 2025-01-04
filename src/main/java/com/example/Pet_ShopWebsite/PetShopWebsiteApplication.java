88bffd6309c2c6c9e5c73b175b3b10918c8de903
Berbentsev <denberben@gmail.com>
2025-01-04 14:52:25 +0400

    Start

---
+++ src/main/java/com/example/Pet_ShopWebsite/PetShopWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pet_ShopWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopWebsiteApplication.class, args);
+	}
+
+}
